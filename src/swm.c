4f312767602c2980f91f66d64156546a8700795b
quispewilmer <wilmerquispecode@gmail.com>
2023-02-15 23:48:24 -0500

    Add X display

--- src/swm.c
+++ src/swm.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h>
+/* X libs */
+#include <X11/Xlib.h>
+
+static Display *dpy;
 
 void 
 die (const char *strerr, ...) 
@@ -16,8 +20,10 @@ int
 main(int argc, char *argv[]) 
 {
     if (argc == 2 && !strcmp("-v", argv[1]))
-        die("swm 0.1");
+        die("swm-0.1\n");
     else if (argc != 1)
-        die("use: swm [-v]");
-    
+        die("swm: use swm [-v]\n");
+    if (!(dpy = XOpenDisplay(NULL)))
+        die("swm: cannot open display\n");
+    /* TODO: Make top-level windows be shown to users */
 }
